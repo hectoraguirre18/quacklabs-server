e7086c70f0f373afa5fd8b19604a7bd6dbb8ed07
UrielGlez <urielglez1011@gmail.com>
2020-11-19 00:29:27 -0700

    materialize framework added

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const fs = require('fs');
 const log4js = require('log4js');
 const pug = require('pug');
 
-
 const logger = log4js.getLogger();
 logger.level = 'debug';
 
 http.createServer((req, res) => {
   if(req.url){
-    if(req.url == '/team') {
+    if(req.url == '/about') {
       fs.readFile('./WWW/team.json', (err, data) => {
         if(err) {
           logger.warn(`NOT FOUND ${req.method} ${req.url}`);
