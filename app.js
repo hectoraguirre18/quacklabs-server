ca5aae24bd862b033124bf5c0865e2537557d6cc
UrielGlez <urielglez1011@gmail.com>
2020-11-18 22:55:43 -0700

    json response added

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 const http = require('http');
 const fs = require('fs');
 const log4js = require('log4js');
+const pug = require('pug');
+
 
 const logger = log4js.getLogger();
 logger.level = 'debug';
 
 http.createServer((req, res) => {
   if(req.url){
-    const file = req.url === '/' ? './WWW/index.html' : `./WWW${req.url}`;
-    fs.readFile(file, (err, data) => {
-      if(err) {
-        logger.warn(`NOT FOUND ${req.method} ${req.url}`);
-        res.writeHead(404, {"Content-Type": "text/html"});
-        res.write('NOT FOUND');
-      } else {
-        logger.info(`OK ${req.method} ${req.url}`);
-        if (file.split(".").pop() == "png")
-          response.writeHead(200, { "Content-Type": "image/png" });
-        else
-          res.writeHead(200, {"Content-Type": "text/html"});
-        res.write(data);
-      }
+    if(req.url == '/team') {
+      fs.readFile('./WWW/team.json', (err, data) => {
+        if(err) {
+          logger.warn(`NOT FOUND ${req.method} ${req.url}`);
+          res.writeHead(404, {"Content-Type": "text/html"});
+          res.write('NOT FOUND');
+        } else {
+          res.writeHead(200, { "Content-Type": "application/json"});
+          res.write(data);
+        }
+        res.end();
+      })
+    }else {
+      const file = req.url === '/' ? './WWW/view/landing.pug' : `./WWW${req.url}`;
+      const pugFile = pug.renderFile(file, { name: 'Quack'});
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.write(pugFile);
       res.end();
-    });
+    }
   }
 }).listen(4000);
