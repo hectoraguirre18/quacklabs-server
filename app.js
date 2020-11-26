cda4b9f9446b63924efa78f183d5a9d1e71f7f5b
Carlos García <a320605@uach.mx>
2020-11-26 16:35:27 -0700

    added carlos cv and languages

--- app.js
+++ app.js
@@ -40,14 +40,52 @@ http.createServer((req, res) => {
         res.write('NOT FOUND');
         res.end();
       }
+    } else if (req.url.split('.').pop() == 'jpg') {
+      try {
+        fs.readFile(`./WWW/assets${req.url}`, (err, data) => {
+          if (err) {
+            logger.warn(`NOT FOUND ${req.method} ${req.url}`);
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.write('NOT FOUND');
+          } else {
+            res.writeHead(200, { "Content-Type": "image/jpg" });
+            res.write(data);
+          }
+          res.end();
+        })
+      } catch (e) {
+        logger.warn(`NOT FOUND ${req.method} ${req.url}`);
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write('NOT FOUND');
+        res.end();
+      }
+    } else if (req.url.split('.').pop() == 'pdf') {
+      try {
+        fs.readFile(`./WWW/assets${req.url}`, (err, data) => {
+          if (err) {
+            logger.warn(`NOT FOUND ${req.method} ${req.url}`);
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.write('NOT FOUND');
+          } else {
+            res.writeHead(200, { "Content-Type": "application/pdf" });
+            res.write(data);
+          }
+          res.end();
+        })
+      } catch (e) {
+        logger.warn(`NOT FOUND ${req.method} ${req.url}`);
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write('NOT FOUND');
+        res.end();
+      }
     } else {
       let pugFile;
       const lastChar = req.url.substr(req.url.length - 1);
-      if(lastChar >= '0' && lastChar <='9') {
+      if (lastChar >= '0' && lastChar <= '9') {
         const file = './WWW/view/profile.pug';
         pugFile = pug.renderFile(file, { teammate: team.equipo[parseInt(lastChar)] });
-        
-      }else {
+
+      } else {
         const file = './WWW/view/landing.pug';
         pugFile = pug.renderFile(file, { team: team.equipo });
       }
